ad1d67df646fcbdf252e81e6fb61f6ec79222a94
DESKTOP-B4L9B2O\Дима <filonenkodmytrodm@gmail.com>
2023-03-14 00:08:36 +0200

    TS-4 Animation

---
+++ Assets/Scripts/Player/AnimationType.cs
@@ -0,0 +1,13 @@
+﻿namespace Player
+{
+    public enum AnimationType
+    {
+        Idle = 0,
+        Run = 1,
+        Jump = 2,
+        Roll = 3,
+        Attack = 4, 
+        Block = 5,
+        Dead = 6,
+    }
+}
--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -7,6 +7,8 @@ namespace Player
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerEntity : MonoBehaviour
     {
+        [SerializeField] private Animator _animator;
+        
         [Header("HorizontalMovement")]
         [SerializeField] private float _horizontalSpeed;
         [SerializeField] private Direction _direction;
@@ -29,6 +31,11 @@ namespace Player
         private float _sizeModificator;
         private bool _isJumping;
         private float _startJumpVerticalPosition;
+
+        private Vector2 _movement;
+        private AnimationType _currentAnimationType;
+        
+        
         void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
@@ -43,10 +50,20 @@ namespace Player
         {
             if(_isJumping)
                 UpdateJump();
+
+            UpdateAnimations();
+        }
+
+        private void UpdateAnimations()
+        {
+            PlayAnimation(AnimationType.Idle, true);
+            PlayAnimation(AnimationType.Run, _movement.magnitude > 0);
+            PlayAnimation(AnimationType.Jump, _isJumping);
         }
 
         public void MoveHorizontally(float direction)
         {
+            _movement.x = direction;
             SetDirection(direction);
             Vector2 velocity = _rigidbody.velocity;
             velocity.x = direction * _horizontalSpeed;
@@ -57,6 +74,8 @@ namespace Player
         {
             if(_isJumping)
                 return;
+
+            _movement.y = direction;
             Vector2 velocity = _rigidbody.velocity;
             velocity.y = direction * _verticalSpeed;
             _rigidbody.velocity = velocity;
@@ -119,6 +138,31 @@ namespace Player
             _rigidbody.position = new Vector2(_rigidbody.position.x, _startJumpVerticalPosition);
             _rigidbody.gravityScale = 0;
         }
+
+        private void PlayAnimation(AnimationType animationType, bool active)
+        {
+            if (!active)
+            {
+                if(_currentAnimationType == AnimationType.Idle || _currentAnimationType != animationType)
+                    return;
+
+                _currentAnimationType = AnimationType.Idle;
+                PlayAnimation(_currentAnimationType);
+                return;
+            }
+            
+            if(_currentAnimationType > animationType)
+                return;
+
+            _currentAnimationType = animationType;
+            PlayAnimation(_currentAnimationType);
+        }
+
+        private void PlayAnimation(AnimationType animationType)
+        {
+            _animator.SetInteger(nameof(AnimationType), (int)animationType);    
+        }
+        
     }
 }
 
